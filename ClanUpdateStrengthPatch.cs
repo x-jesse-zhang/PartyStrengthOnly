b66a00f455a58778d7b770354fb7d2f671c296a3
Jesse Zhang <macisasandwich@gmail.com>
2022-10-20 18:25:24 -0700

    Add project files.

---
+++ ClanUpdateStrengthPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.Clan;
+using HarmonyLib;
+
+namespace PartyStrengthOnly
+{
+    [HarmonyPatch(typeof(Clan), nameof(UpdateStrength))]
+    internal class ClanUpdateStrengthPatch
+    {
+        static bool Prefix()
+        {
+            this.TotalStrength = 0f;
+            foreach (WarPartyComponent warPartyComponent in this._warPartyComponentsCache)
+            {
+                this.TotalStrength += warPartyComponent.MobileParty.Party.TotalStrength;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,27 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+
+namespace PartyStrengthOnly
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony h = new Harmony("PartyStrengthOnly");
+            h.PatchAll();
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
